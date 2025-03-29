978855d1dbe8d0175ea1e5e547f91b08cf1081bb
fjc-22233580 <22233580@bucks.ac.uk>
2025-03-29 14:17:29 +0000

    Added class wrapper to entry point

--- BNUStockMate/Program.cs
+++ BNUStockMate/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace BNUStockMate;
+
+/// <summary>
+/// Entry point into BNUStockMate
+/// </summary>
+public static class Program
+{
+    /// <summary>
+    /// App starting point
+    /// </summary>
+    /// <param name="args"> Command line args </param>
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
