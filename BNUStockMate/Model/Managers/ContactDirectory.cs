7ca9aaca7c655bc6d05e6220424959e3cd4ddf85
fjc-22233580 <22233580@bucks.ac.uk>
2025-05-30 21:16:30 +0100

    Models commented

--- BNUStockMate/Model/Managers/ContactDirectory.cs
+++ BNUStockMate/Model/Managers/ContactDirectory.cs
@@ -2,11 +2,25 @@ using BNUStockMate.Model.Info;
 
 namespace BNUStockMate.Model.Managers;
 
+/// <summary>
+/// Represents a directory for managing contacts: suppliers and customers.
+/// Allows adding, removing, and retrieving contacts.
+/// </summary>
 public class ContactDirectory
 {
+    /// <summary>
+    /// Represents a collection of suppliers.
+    /// </summary>
     private readonly List<Supplier> _suppliers = new List<Supplier>();
+
+    /// <summary>
+    /// Represents a collection of customers.
+    /// </summary>
     private readonly List<Customer> _customers = new List<Customer>();
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ContactDirectory"/> class.
+    /// </summary>
     public ContactDirectory()
     {
         _suppliers.Add(new Supplier(123, "CompanyA", "email@email.com", "132-123"));
@@ -15,11 +29,23 @@ public class ContactDirectory
         _customers.Add(new Customer(5686, "Burger King", "info@BK.com", "4532-657"));
     }
 
+    /// <summary>
+    /// Gets the collection of suppliers in our system.
+    /// </summary>
     public List<Supplier> Suppliers => _suppliers;
     
+    /// <summary>
+    /// Gets the collection of customers in our system.
+    /// </summary>
     public List<Customer> Customers => _customers;
     
-
+    /// <summary>
+    /// Adds a new supplier to the collection and returns the created supplier.
+    /// </summary>
+    /// <param name="name">The name of the supplier. Cannot be null or empty.</param>
+    /// <param name="email">The email address of the supplier. Must be a valid email format.</param>
+    /// <param name="phone">The phone number of the supplier. Cannot be null or empty.</param>
+    /// <returns>A <see cref="Supplier"/> object representing the newly added supplier.</returns>
     public Supplier AddSupplier(string name, string email, string phone)
     {
         int id = _suppliers.Count + 1;
@@ -28,6 +54,13 @@ public class ContactDirectory
         return supplier;
     }
 
+    /// <summary>
+    /// Adds a new customer to the collection and returns the created customer.
+    /// </summary>
+    /// <param name="name">The name of the customer. Cannot be null or empty.</param>
+    /// <param name="email">The email address of the customer. Cannot be null or empty.</param>
+    /// <param name="phone">The phone number of the customer. Cannot be null or empty.</param>
+    /// <returns>The newly created <see cref="Customer"/> instance.</returns>
     public Customer AddCustomer(string name, string email, string phone)
     {
         int id = _customers.Count + 1;
@@ -36,24 +69,22 @@ public class ContactDirectory
         return customer;
     }
 
+    /// <summary>
+    /// Removes the specified customer from the collection of customers.
+    /// </summary>
+    /// <param name="customer">The customer to remove. </param>
     public void RemoveCustomer(Customer customer)
     {
         _customers.Remove(customer);
     }
 
+    /// <summary>
+    /// Removes the specified suppler from the collection of supplier.
+    /// </summary>
+    /// <param name="supplier">The supplier to remove. </param>
     public void RemoveSupplier(Supplier supplier)
     {
         _suppliers.Remove(supplier);
-    }
-
-    //public Supplier? FindSupplierById(string id)
-    //{
-    //    return _suppliers.FirstOrDefault(s => s.Id == id);
-    //}
-
-    public Customer? FindCustomerById(int id)
-    {
-        return _customers.FirstOrDefault(s => s.Id == id);
     }
 
 }
