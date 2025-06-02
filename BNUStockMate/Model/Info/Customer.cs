cbe8ac9cd26a4ddd109b9037229e794526197ecd
fjc-22233580 <22233580@bucks.ac.uk>
2025-06-02 08:36:10 +0100

    Added more comments

--- BNUStockMate/Model/Info/Customer.cs
+++ BNUStockMate/Model/Info/Customer.cs
@@ -1,11 +1,32 @@
 namespace BNUStockMate.Model.Info;
 
+/// <summary>
+/// Represents a customer with an identifier, name, email, and phone number.
+/// </summary>
 public class Customer
 {
+    /// <summary>
+    /// The customer name.
+    /// </summary>
     private readonly string _name;
+
+    /// <summary>
+    /// The customer email address.
+    /// </summary>
     private readonly string _email;
+
+    /// <summary>
+    /// The customer phone number.
+    /// </summary>
     private readonly string _phone;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Customer"/> class with the specified customer details.
+    /// </summary>
+    /// <param name="id">The unique identifier for the customer.</param>
+    /// <param name="name">The name of the customer. Cannot be null or empty.</param>
+    /// <param name="email">The email address of the customer. Must be a valid email format.</param>
+    /// <param name="phone">The phone number of the customer. Cannot be null or empty.</param>
     public Customer(int id, string name, string email, string phone)
     {
         Id = id;
@@ -14,10 +35,20 @@ public class Customer
         _phone = phone;
     }
     
+    /// <summary>
+    /// Gets the unique identifier for the entity.
+    /// </summary>
     public int Id { get; }
 
+    /// <summary>
+    /// Gets the name associated with the current instance.
+    /// </summary>
     public string Name => _name;
 
+    /// <summary>
+    /// Overrides the ToString method to provide a string representation of the customer.
+    /// </summary>
+    /// <returns></returns>
     public override string ToString()
     {
         return $"ID: {Id} - {_name} - {_email} - {_phone}";
--- BNUStockMate/Model/Info/Supplier.cs
+++ BNUStockMate/Model/Info/Supplier.cs
@@ -1,12 +1,37 @@
 namespace BNUStockMate.Model.Info;
 
+/// <summary>
+/// Represents a supplier with identifying information, including name, email, and phone number.
+/// </summary>
 public class Supplier
 {
+    /// <summary>
+    /// The unique identifier for the supplier.
+    /// </summary>
     private readonly int _id;
+
+    /// <summary>
+    /// The name of the supplier.
+    /// </summary>
     private readonly string _name;
+
+    /// <summary>
+    /// The email address of the supplier.
+    /// </summary>
     private readonly string _email;
+
+    /// <summary>
+    /// The phone number of the supplier.
+    /// </summary>
     private readonly string _phone;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Supplier"/> class with the specified supplier details.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <param name="name"></param>
+    /// <param name="email"></param>
+    /// <param name="phone"></param>
     public Supplier(int id, string name, string email, string phone)
     {
         _id = id;
@@ -15,10 +40,21 @@ public class Supplier
         _phone = phone;
     }
     
+    /// <summary>
+    /// Gets the unique identifier for the current instance.
+    /// </summary>
     public int Id => _id;
 
+    /// <summary>
+    /// Gets the name associated with the current instance.
+    /// </summary>
     public string Name => _name;
 
+    /// <summary>
+    /// Returns a string representation of the object, including its ID, name, email, and phone number.
+    /// </summary>
+    /// <returns>A string containing the object's ID, name, email, and phone number in the format: "ID: [ID] - [Name] - [Email] -
+    /// [Phone]".</returns>
     public override string ToString()
     {
         return $"ID: {_id} - {_name} - {_email} - {_phone}";
